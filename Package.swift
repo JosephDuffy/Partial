b784d5ffc329f2d60be1d14b6f365eb4d7e30341
Joseph Duffy <commits@josephduffy.co.uk>
2019-05-15 23:05:15 +0100

    Add Travis CI, with deployment and tests

--- Package.swift
+++ Package.swift
@@ -7,6 +7,7 @@ let package = Package(
         .library(name: "Partial", type: .dynamic, targets: ["Partial"]),
     ],
     dependencies: [
+        .package(url: "https://github.com/danger/swift.git", from: "1.0.0"),
         .package(url: "https://github.com/Quick/Quick.git", from: "2.0.0"),
         .package(url: "https://github.com/Quick/Nimble.git", from: "8.0.0"),
     ],
---
+++ Resources/DangerDeps.swift
@@ -0,0 +1,1 @@
+// An empty file to allow SPM to build Danger
