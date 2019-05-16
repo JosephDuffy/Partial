326262f9a0f87b3502f335c350d96ed23a5488f7
Joseph Duffy <commits@josephduffy.co.uk>
2019-05-16 16:58:57 +0100

    Fix nested partials preventing property removal

--- Source/Partial.swift
+++ Source/Partial.swift
@@ -1,22 +1,22 @@
 import Foundation
 
 public struct Partial<Wrapped>: CustomStringConvertible, CustomDebugStringConvertible {
-    
+
     public enum Error<Value>: Swift.Error {
         case missingKey(KeyPath<Wrapped, Value>)
         case invalidValue(key: KeyPath<Wrapped, Value>, actualValue: Any?)
     }
-    
+
     internal var values: [PartialKeyPath<Wrapped>: Any?] = [:]
-    
+
     internal var backingValue: Wrapped? = nil
-    
+
     internal var updateHandlers: [PartialKeyPath<Wrapped>: [(Any?) -> Void]] = [:]
-    
+
     public init(backingValue: Wrapped? = nil) {
         self.backingValue = backingValue
     }
-    
+
     public func value<Value>(for key: KeyPath<Wrapped, Value>) throws -> Value {
         if let value = values[key] {
             if let value = value as? Value {
@@ -27,10 +27,10 @@ public struct Partial<Wrapped>: CustomStringConvertible, CustomDebugStringConver
         } else if let value = backingValue?[keyPath: key] {
             return value
         }
-        
+
         throw Error.missingKey(key)
     }
-    
+
     public func value<Value>(for key: KeyPath<Wrapped, Value?>) throws -> Value {
         if let value = values[key] {
             if let value = value as? Value {
@@ -41,15 +41,15 @@ public struct Partial<Wrapped>: CustomStringConvertible, CustomDebugStringConver
         } else if let value = backingValue?[keyPath: key] {
             return value
         }
-        
+
         throw Error.missingKey(key)
     }
-    
+
     public mutating func set<Value>(value: Value, for key: KeyPath<Wrapped, Value>) {
         values[key] = value
         updateHandlers[key]?.forEach { $0(value as Any) }
     }
-    
+
     public mutating func set<Value>(value: Value?, for key: KeyPath<Wrapped, Value?>) {
         /**
          Uses `updateValue(_:forKey:)` to ensure the value is set to `nil`, rather than
@@ -60,12 +60,12 @@ public struct Partial<Wrapped>: CustomStringConvertible, CustomDebugStringConver
         values.updateValue(value, forKey: key)
         updateHandlers[key]?.forEach { $0(value as Any) }
     }
-    
+
     public mutating func removeValue<Value>(for key: KeyPath<Wrapped, Value>) {
         values.removeValue(forKey: key)
         updateHandlers[key]?.forEach { $0(nil) }
     }
-    
+
     public subscript<Value>(key: KeyPath<Wrapped, Value>) -> Value? {
         get {
             return try? value(for: key)
@@ -74,7 +74,7 @@ public struct Partial<Wrapped>: CustomStringConvertible, CustomDebugStringConver
             values.updateValue(newValue, forKey: key)
         }
     }
-    
+
     public subscript<Value>(key: KeyPath<Wrapped, Value?>) -> Value? {
         get {
             return try? value(for: key)
