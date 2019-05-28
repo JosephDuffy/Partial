7f1d891902316a3a3a7081a7718062f319faed9e
Joseph Duffy <commits@josephduffy.co.uk>
2019-05-29 00:21:48 +0100

    Remove Rocket, update danger-swift
    This config fixes a timeout when checking out dependencies, maybe Rocket can be-added

--- Package.swift
+++ Package.swift
@@ -8,7 +8,6 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/danger/swift.git", from: "1.0.0"), // dev
-        .package(url: "https://github.com/f-meloni/Rocket", from: "0.1.0"), // dev
         .package(url: "https://github.com/Quick/Quick.git", from: "2.0.0"), // dev
         .package(url: "https://github.com/Quick/Nimble.git", from: "8.0.0"), // dev
         .package(url: "https://github.com/Realm/SwiftLint", from: "0.32.0"), // dev
