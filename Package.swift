3cb670277423c166669023006b4bfaa75280bf86
Joseph Duffy <commits@josephduffy.co.uk>
2019-07-06 18:05:54 +0100

    Build libDanger via SwiftPM
    danger-swift brew package is no longer required because danger will use the libDanger being built by SwiftPM

--- Package.swift
+++ Package.swift
@@ -18,6 +18,7 @@ let package = Package(
     targets: [
         .target(name: "Partial"),
         .testTarget(name: "PartialTests", dependencies: ["Partial", "Quick", "Nimble"]),
+        .target(name: "CIDependencies", dependencies: ["Danger", "swiftlint"], path: "Resources"), // dev
     ],
     swiftLanguageVersions: [.v5]
 )
