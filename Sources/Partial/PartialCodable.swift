4908a8bc758b9b66a208ddfb37e3c59d805e7eab
Joseph Duffy <commits@josephduffy.co.uk>
2021-01-27 20:09:08 +0000

    Remove `Codable` requirement from `PartialCodable`

--- Sources/Partial/PartialCodable.swift
+++ Sources/Partial/PartialCodable.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-public protocol PartialCodable: Codable {
+public protocol PartialCodable {
     associatedtype CodingKey: Swift.CodingKey
 
     static func encodeValue(_ value: Any, for keyPath: PartialKeyPath<Self>, to container: inout KeyedEncodingContainer<CodingKey>) throws
