b57c2152d6b8b6973711551fabfb71adec3b7fad
Joseph Duffy <commits@josephduffy.co.uk>
2021-06-19 23:48:19 +0100

    Default to Swift >= 5.3

--- Package@swift-5.1.swift
+++ Package@swift-5.1.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
     name: "Partial",
     platforms: [
-        .macOS(.v10_10), .iOS(.v9), .tvOS(.v9), .watchOS(.v2),
+        .macOS(.v10_10), .iOS(.v8), .tvOS(.v9), .watchOS(.v2),
     ],
     products: [
         .library(name: "Partial", targets: ["Partial"]),
