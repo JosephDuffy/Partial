7dba9132cdb94ff11f881a144f92a8af0250a06b
Joseph Duffy <commits@josephduffy.co.uk>
2022-02-04 20:04:09 -0800

    Replace jazzy with DocC

---
+++ scripts/generate-documentation.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+/Library/Developer/Toolchains/swift-5.6-DEVELOPMENT-SNAPSHOT-2022-01-11-a.xctoolchain/usr/bin/swift package \
+    --allow-writing-to-directory "$1" \
+    --target Partial \
+    generate-documentation \
+    --disable-indexing \
+    --output-path "$1" \
+    --transform-for-static-hosting \
+    --hosting-base-path Partial
