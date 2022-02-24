9350f959afa5a1998bf83aa425f169055f84e11a
Joseph Duffy <commits@josephduffy.co.uk>
2022-02-24 14:01:34 +0000

    Require output directory to be provided

--- scripts/generate-documentation.sh
+++ scripts/generate-documentation.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 export DEVELOPER_DIR=/Applications/Xcode_13.3-beta.3.app/
+
+if [ "$#" -lt 1 ]; then
+    echo "Usage: $0 <directory>" >&2
+     exit 1
+fi
+
 swift package \
     --allow-writing-to-directory "$1" \
     generate-documentation \
